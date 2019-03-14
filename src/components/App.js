5f61aa86540862597187857ec178230be071614f
Thales Avila <thalespizza@gmail.com>
2019-03-14 10:48:26 -0300

    Implementando controlled component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-	return <SearchBar />;
-};
+class App extends React.Component {
+  state = {query:''}
+  
+  onSearchSubmit(term) {
+    console.log(term);
+    
+  }
+  
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = {term:''};
+
+  onFormSubmit = (e) => {
+    e.preventDefault(); 
+    this.props.onSubmit(this.state.term);      
+  }
+
 	render() {
 		return (
-			<div>
-				<form action="">
-					<input type="text" name="" id="" />
+			<div className="ui segment">
+				<form onSubmit={this.onFormSubmit} action="" className="ui form">
+					<div className="field">
+						<label htmlFor="search">Image Search</label>
+						<input type="text" name="search" id="search" value={this.state.term} onChange={(e)=>{this.setState({term:e.target.value})}} />
+					</div>
 				</form>
 			</div>
 		);
