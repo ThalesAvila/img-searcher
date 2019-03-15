b7168104cfea25e89ac72ecc1efdddf157490b6b
Thales Avila <thalespizza@gmail.com>
2019-03-15 15:44:39 -0300

    Adicionando lista de imagens

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-	state = { term: '' };
+  state = { term: '' };
 
-	onFormSubmit = (e) => {
-		e.preventDefault();
-		this.props.onSubmit(this.state.term);
-	};
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
 
-	render() {
-		return (
-			<div className="ui segment">
-				<form onSubmit={this.onFormSubmit} action="" className="ui form">
-					<div className="field">
-						<label htmlFor="search">Image Search</label>
-						<input
-							type="text"
-							name="search"
-							id="search"
-							value={this.state.term}
-							onChange={(e) => {
-								this.setState({ term: e.target.value });
-							}}
-						/>
-					</div>
-				</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} action="" className="ui form">
+          <div className="field">
+            <label htmlFor="search">Image Search</label>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              value={this.state.term}
+              onChange={e => {
+                this.setState({ term: e.target.value });
+              }}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
