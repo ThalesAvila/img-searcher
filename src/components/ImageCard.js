f491c186641ea1f0a5106b7b199bab445ec8c973
Thales Avila <thalespizza@gmail.com>
2019-03-15 22:52:46 -0300

    Implementando React refs para o ImageCard component

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img src={description} alt={urls} />
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
